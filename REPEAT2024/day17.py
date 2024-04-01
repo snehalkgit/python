201ccc25cdb06330a5cd18d175885786ecbc2dfd
snehal kamble <you@example.com>
2024-04-01 13:24:54 +0530

    Added

---
+++ REPEAT2024/day17.py
@@ -0,0 +1,31 @@
+# compile - time error syntax error
+
+# def addition(x,y):
+# print(x+y)
+# addition(12,3)
+
+##run time error
+# print("snehal")
+# x = int(input('enter the number one'))
+# y = int(input('enter the number two '))
+# print(x/y)
+# print("bye")
+#
+# ##logical error
+# def add10Pr(amount):
+#     return amount * 0.20
+# e = add10Pr(100)
+# print(e)
+
+
+# program 3
+print("hello")
+try:
+    x = int(input('Enter the number A'))
+    y = int(input('Enter the number B'))
+    print(x/y)
+except Exception:
+    print("invalid input")
+print('good-bye')
+
+
